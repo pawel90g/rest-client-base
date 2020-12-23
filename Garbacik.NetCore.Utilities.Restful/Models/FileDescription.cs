caa8afdc4a6f0b62cd615b53a3cc92d1ffde28bf
Paweł Garbacik <pawel90g@outlook.com>
2020-12-23 15:13:02 +0100

    Added FileDescription constructors -> default FileDescription(byte[] fileBytes, string fileName) and FileDescription(IFormFile formFile)

--- Garbacik.NetCore.Utilities.Restful/Models/FileDescription.cs
+++ Garbacik.NetCore.Utilities.Restful/Models/FileDescription.cs
@@ -1,8 +1,31 @@
-﻿namespace Garbacik.NetCore.Utilities.Restful.Models
+﻿using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace Garbacik.NetCore.Utilities.Restful.Models
 {
     public class FileDescription
     {
-        public byte[] FileBytes { get; set; }
-        public string FileName { get; set; }
+        public byte[] FileBytes { get; private set; }
+        public string FileName { get; private set; }
+
+        public FileDescription(byte[] fileBytes, string fileName)
+        {
+            FileBytes = fileBytes;
+            FileName = fileName;
+        }
+
+        public FileDescription(IFormFile formFile)
+        {
+            FileName = formFile.FileName;
+
+            if (formFile.Length > 0)
+            {
+                using (var ms = new MemoryStream())
+                {
+                    formFile.CopyTo(ms);
+                    FileBytes = ms.ToArray();
+                }
+            }
+        }
     }
 }
