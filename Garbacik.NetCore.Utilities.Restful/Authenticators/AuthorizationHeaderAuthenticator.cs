108852a7ab3f2a5dc27bcd6c7b01666dac531bc3
Paweł Garbacik <pawel90g@outlook.com>
2021-05-05 08:31:58 +0200

    AuthorizationHeaderAuthenticator.cs

---
+++ Garbacik.NetCore.Utilities.Restful/Authenticators/AuthorizationHeaderAuthenticator.cs
@@ -0,0 +1,20 @@
+﻿using RestSharp;
+using RestSharp.Authenticators;
+
+namespace Garbacik.NetCore.Utilities.Restful.Authenticators
+{
+    public class AuthorizationHeaderAuthenticator : IAuthenticator
+    {
+        private readonly string authorizationHeaderValue;
+        
+        public AuthorizationHeaderAuthenticator(string authorizationHeaderValue)
+        {
+            this.authorizationHeaderValue = authorizationHeaderValue;
+        }
+        
+        public void Authenticate(IRestClient client, IRestRequest request)
+        {
+            request.AddHeader("Authorization", authorizationHeaderValue);
+        }
+    }
+}
