ba3a070ef6d58d65cefac6477759ea03f65edaa1
Paweł Garbacik <garbacik.pawel@gmail.com>
2022-04-27 20:05:09 +0200

    String extensions

---
+++ Garbacik.NetCore.Utilities.Restful.Tests/StringExtensionsTests.cs
@@ -0,0 +1,61 @@
+﻿using Garbacik.NetCore.Utilities.Restful.Extensions;
+using Xunit;
+
+namespace Garbacik.NetCore.Utilities.Restful.Tests;
+
+public class StringExtensionsTests
+{
+    [Fact]
+    public void IsUpperTest()
+    {
+        Assert.True("A".IsUpper());
+        Assert.False("a".IsUpper());
+        Assert.True('A'.IsUpper());
+        Assert.False('a'.IsUpper());
+    }
+    
+    [Fact]
+    public void IsLowerTest()
+    {
+        Assert.False("A".IsLower());
+        Assert.True("a".IsLower());
+        Assert.False('A'.IsLower());
+        Assert.True('a'.IsLower());
+    }
+
+    [Fact]
+    public void SplitByUpperCharTests()
+    {
+        var split = "aaaBBcccDaasdasdFFFRDFadcD".SplitByUpperChar();
+        Assert.Equal(11, split.Length);
+        Assert.Equal("aaa", split[0]);
+        Assert.Equal("B", split[1]);
+        Assert.Equal("Bccc", split[2]);
+        Assert.Equal("Daasdasd", split[3]);
+        Assert.Equal("F", split[4]);
+        Assert.Equal("F", split[5]);
+        Assert.Equal("F", split[6]);
+        Assert.Equal("R", split[7]);
+        Assert.Equal("D", split[8]);
+        Assert.Equal("Fadc", split[9]);
+        Assert.Equal("D", split[10]);
+    }
+
+    [Theory]
+    [InlineData("ThisIsPascalCase", "this_is_pascal_case")]
+    [InlineData("ThisIsPascalCase", "thisIsPascalCase")]
+    [InlineData("ThisIsPascalCase", "ThisIsPascalCase")]
+    public void ToPascalCaseTests(string expected, string input)
+    {
+        Assert.Equal(expected, input.ToPascalCase());
+    }
+
+    [Theory]
+    [InlineData("thisIsCamelCase", "this_is_camel_case")]
+    [InlineData("thisIsCamelCase", "thisIsCamelCase")]
+    [InlineData("thisIsCamelCase", "ThisIsCamelCase")]
+    public void ToCamelCaseTests(string expected, string input)
+    {
+        Assert.Equal(expected, input.ToCamelCase());
+    }
+}
--- Garbacik.NetCore.Utilities.Restful/Extensions/StringExtensions.cs
+++ Garbacik.NetCore.Utilities.Restful/Extensions/StringExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Garbacik.NetCore.Utilities.Restful.Extensions;
 
@@ -8,11 +10,98 @@ public static class StringExtensions
     {
         foreach (KeyValuePair<string, string> pair in replaceParts)
         {
-            source = pair.Key.StartsWith("{") && pair.Key.EndsWith("}") ?
-                source.Replace(pair.Key, pair.Value) :
-                source.Replace($"{{{pair.Key}}}", pair.Value);
+            source = pair.Key.StartsWith("{") && pair.Key.EndsWith("}")
+                ? source.Replace(pair.Key, pair.Value)
+                : source.Replace($"{{{pair.Key}}}", pair.Value);
         }
 
         return source;
     }
+
+    public static string ToPascalCase(this string source)
+    {
+        if (string.IsNullOrWhiteSpace(source))
+            return source;
+
+        var split = source
+            .Split('_')
+            .Where(word => !string.IsNullOrWhiteSpace(word))
+            .SelectMany(x => x.SplitByUpperChar())
+            .ToList();
+
+        var pascalCaseWords = split
+            .Select(word =>
+            {
+                if (word.Length == 1)
+                    return word
+                        .ToUpper();
+
+                return word
+                           .ElementAt(0)
+                           .ToString()
+                           .ToUpper() +
+                       word[1..]
+                           .ToLower();
+            });
+
+        return string.Join(string.Empty, pascalCaseWords);
+    }
+
+    public static string[] SplitByUpperChar(this string source)
+    {
+        var words = new List<string>();
+        var word = string.Empty;
+        
+        for (var charIndex = 0; charIndex < source.Length; charIndex++)
+        {
+            var c = source[charIndex];
+            if (charIndex == 0 || c.IsUpper())
+            {
+                if(!string.IsNullOrWhiteSpace(word))
+                    words.Add(word);
+
+                word = c.ToString();
+            }
+            else
+            {
+                word += c.ToString();
+            }
+        }
+        
+        if(!string.IsNullOrWhiteSpace(word))
+            words.Add(word);
+
+        return words.ToArray();
+    }
+
+    public static string ToCamelCase(this string source)
+    {
+        if (string.IsNullOrWhiteSpace(source))
+            return source;
+
+        var pascalCase = source
+            .ToPascalCase();
+
+        return pascalCase.Length == 1
+            ? pascalCase.ToLower()
+            : pascalCase
+                  .ElementAt(0)
+                  .ToString()
+                  .ToLower() +
+              pascalCase[1..];
+    }
+
+    public static bool IsUpper(this char c) =>
+        c.ToString().ToUpper() == c.ToString();
+
+
+    public static bool IsUpper(this string s) =>
+        s.ToUpper() == s;
+
+    public static bool IsLower(this char c) =>
+        c.ToString().ToLower() == c.ToString();
+
+
+    public static bool IsLower(this string s) =>
+        s.ToLower() == s;
 }
